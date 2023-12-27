5ed11e39e2516f0d47a70dd62e00412551c8da20
Rathes <mailramratheesh47@gmail.com>
2023-12-28 02:28:55 +0530

    Added image extraction functionality

--- file_extraction/content_extractor.py
+++ file_extraction/content_extractor.py
@@ -1,18 +1,16 @@
 import fitz
 from pdf2docx import Converter
 from response_transition import TextractAnalyzer, LayoutFigureIdentifier, FontSizeEstimator  # Import the new classes
+from crop_image_pdf import LayoutFigureGeometry, PDFImagePaster
 
-pdf_path = 'hello.pdf'
+pdf_path = 'hello2.png'
 
-# Initialize the TextractAnalyzer and get the Textract response
 analyzer = TextractAnalyzer()
 textract_response = analyzer.get_response_from_textract(pdf_path)
 
-# Find layout figure IDs using the LayoutFigureIdentifier class
 layout_figure_ids = LayoutFigureIdentifier.find_layout_figure_ids(textract_response['Blocks'])
 
-# Create a new PDF document for the output
-output_pdf = fitz.open()  # Create a new, empty PDF document
+output_pdf = fitz.open()
 
 # Add a new page to the output PDF with dimensions of a standard US letter size page
 page_width, page_height = 595, 842  # Width and height of a standard US letter size page in points
@@ -32,15 +30,21 @@ for block in textract_response['Blocks']:
         else:
             pass
 
-# Save the new PDF with the provided text
 output_pdf_path = 'output_manual_text.pdf'
 output_pdf.save(output_pdf_path)
 print("New PDF with provided text has been saved.")
 
-# Convert the new PDF to a Word document using pdf2docx
-docx_path = "output_pdftodocx_file.docx"  # Define the path for the output DOCX file
-cv = Converter(output_pdf_path)  # Initialize the converter with the path to the new PDF
-cv.convert(docx_path)  # Convert the PDF to DOCX
-cv.close()  # Close the converter
 
-print("PDF has been converted to DOCX and saved.")
+geometry_extractor = LayoutFigureGeometry(textract_response)
+layout_figures = geometry_extractor.get_layout_figures_geometry()
+
+image_paster = PDFImagePaster(scanned_pdf_path=pdf_path, target_pdf_path=output_pdf_path)
+image_paster.paste_cropped_images(layout_figures)
+
+print("Process completed.")
+
+updated_pdf_path = 'updated_' + output_pdf_path
+docx_path = "updated_output_manual_text.docx"
+cv = Converter(updated_pdf_path)
+cv.convert(docx_path)
+cv.close()
